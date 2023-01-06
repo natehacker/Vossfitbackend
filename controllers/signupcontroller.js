b514a82de9f1a3f6e346d02ddf7d6cc4de0b8127
Nathan louissaint <louissaintmark@gmail.com>
2023-01-05 21:05:28 -0500

    started login functionailty

--- controllers/signupcontroller.js
+++ controllers/signupcontroller.js
@@ -8,4 +8,31 @@ async function getAllSignUps(req,res){
       throw error
    }
 }
+const Login = async (req, res) => {
+   try {
+     const user = await SignUp.findOne({
+       where: { username: req.body.username },
+       raw: true
+     })
+     if (
+       user &&
+       (await middleware.comparePassword(user.password, req.body.password))
+     ) {
+       let payload = {
+         id: user.id,
+         username: user.username,
+         password: user.password,
+         comfrimpassword:user.comfrimpassword,
+         email: user.email
+       }    
+       return res.send({ user: payload})
+     }
+     res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+   } catch (error) {
+     throw error
+   }
+ }
 module.exports = {getAllSignUps}
+// still have to create middle wear for login and sign up  
+//create a route for login end point login and sign up
+//
